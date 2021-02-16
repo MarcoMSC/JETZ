113c89884e8473a6a5def1ad27306976c34c447e
Marco Scherrer <marco.scherrer@kinisi.ch>
2021-02-16 18:25:09 +0100

    changed to with socket() as s

--- chat_client.py
+++ chat_client.py
@@ -2,16 +2,16 @@ import socket
 
 port = 8080
 
-s=socket.socket()
-pc_name = input("Server name eingeben: ")
+with socket.socket() as s:
+    pc_name = input("Server name eingeben: ")
 
 
-s.connect((pc_name,port))
-print("Verbunden mit Server")
+    s.connect((pc_name,port))
+    print("Verbunden mit Server")
 
-while 1: 
-    nachricht_empf=s.recv(1024)
-    print(":: ", nachricht_empf.decode())
+    while 1: 
+        nachricht_empf=s.recv(1024)
+        print(":: ", nachricht_empf.decode())
 
-    nachricht= input(">>")
-    s.send(nachricht.encode())
+        nachricht= input(">>")
+        s.sendall(nachricht.encode())
--- chat_server.py
+++ chat_server.py
@@ -2,22 +2,21 @@ import socket
 
 port = 8080
 
-s=socket.socket()
-pc_name = socket.gethostname()
-print("Server gestartet auf: ", pc_name)
+with socket.socket() as s:
+    ip = socket.gethostbyname(socket.gethostname())
+    print("Server gestartet auf: ", ip)
+    s.bind(('', port))
+    s.listen(1)
+    print("Server wartet auf Anfrage...")
 
+    connection,address= s.accept()
+    with connection:
+        print(address, " hat sich verbunden.")
+        connection.send(b"Hallo")
 
-s.bind(('', port))
-s.listen(1)
-print("Server wartet auf Anfrage...")
+        while 1: 
+            nachricht_empf = connection.recv(1024)
+            print(":: ", nachricht_empf.decode())
 
-connection,address= s.accept()
-print(address, " hat sich verbunden.")
-connection.send(b"Hallo")
-
-while 1: 
-    nachricht_empf = connection.recv(1024)
-    print(":: ", nachricht_empf.decode())
-
-    nachricht = input(">>")
-    connection.send(nachricht.encode())
+            nachricht = input(">>")
+            connection.send(nachricht.encode())
